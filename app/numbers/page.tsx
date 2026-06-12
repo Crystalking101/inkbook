40fc6f7905e57c01af9467aea2051985ee6a9e26
Crystal King <crystalking@macbookpro.myfiosgateway.com>
2026-06-11 21:41:02 -0400

    fix unused useEffect import in numbers page

--- app/numbers/page.tsx
+++ app/numbers/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const NUMBERS = [
